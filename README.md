e4a0f2c818d7ae1b883479ca645747220867e85f
Sumukh Barve <8079593+sumukhbarve@users.noreply.github.com>
2020-10-28 22:41:51 +0530

    Hello, World!

---
+++ README.md
@@ -0,0 +1,222 @@
+PogoDB
+=======
+
+Simple NoSQL wrapper for Postgres' JSONB type. 
+
+Installation
+--------------
+As of writing, here's the installation procedure:
+1. `pip install dotsi`
+2. `pip install psycopg2-binary` (or `pip install psycopg2`)
+3. Download `pogodb.py` to your project directory.
+
+We'll work toward delivering a smoother installation experience. In the mean time, please follow the steps above.
+
+
+Quick Start:
+---------------
+To connect from a Python Shell, use `pogodb.shellConnect()`.
+```py
+>>> import pogodb
+>>> db = pogodb.shellConnect("postgres://..dsn..")
+Connection opened. Call `.close()` to close.
+>>> db.insertOne({"_id": "foo", "value": "foobar"})
+>>> db.findOne("foo")
+{'_id': 'foo', 'value': 'foobar'}
+>>> db.close()
+Connection committed & closed. Call `.reopen()` to resume.
+>>>
+```
+*Note:* `pogodb.shellConnect(.)` is meant only for *quick and dirty* shell connections. You need to explicitly call `db.close()` to commit the transaction and close the connection.
+
+Connecting Properly:
+--------------------------
+
+**Context Manager:**
+
+Using `with pogodb.connect(.) as db` is a better way to connect. On exiting the `with` block, the transaction is auto-committed and the connection is auto-closed.
+```py
+import pogodb
+with pogodb.connect("postgres://..dsn..") as db:
+    db.insertOne({"_id": "bar", "value": "foobar"})
+    # etc. ...
+```
+
+**Connection Decorator:**
+For frequently connecting to the same database, consider setting up a connection decorator as follows:
+```py
+import pogodb
+dbConnect = pogodb.makeConnector("postgres://..dsn..")
+
+@dbConnect
+def yourLogic (db):
+    db.insertOne({"_id": "baz", "value": "quax"})
+    # etc. ...
+```
+The decorator supplies the `db` parameter to the decorated function. The parameter is supplied by name, so it must be called `db`, not `myDb` or something else.
+
+
+Inserting Data:
+--------------------------
+```py
+# Insert a single document:
+db.insertOne({
+   "_id":"a", "author":"Alice", "text":"AA", "rank":0,
+})
+# Insert multiple documents:
+db.insertMany([
+  {"_id":"b", "author":"Becci", "text":"BB", "rank":1},
+  {"_id":"c", "author":"Cathy", "text":"CC", "rank":2},
+  {"_id":"d", "author":"Alice", "text":"DD", "rank":1},
+]);
+```
+
+**Document Model:**  
+Each document must:
+- be a JSON-serializable `dict` or dict-like object, *and*
+- have a *unique string* value corresponding to the `"_id"` key.
+
+Retrieving Data:
+--------------------
+In continuation with the above code snippet ...
+```py
+# Find by _id:
+taskA = db.findOne("a");
+print(taskA.author, "-", taskA.text)
+# Output: Alice - AA
+
+# Find by sub-document:
+taskB = db.findOne({"author": "Becci"});
+print(taskB.author, "-", taskB.text)
+# Output: Becci - BB
+
+# Find multiple:
+aliceTasks = db.find({"author": "Alice"})
+assert aliceTasks[0] == taskA and len(aliceTasks) == 2
+taskD = aliceTasks[1];
+print(taskD.author, "-", taskD.text)
+# Output: Alice - AA
+```
+*Note:* If no matching document is found, `.findOne(.)` returns `None` while `.find(.)` returns an empty list.
+
+Updating Data:
+-------------------
+In continuation with the above code snippet ...
+```py
+# Replace document:
+taskA.text = "New AA"           # <-- Update in-memory
+taskA.x = {"y": 10, "z": 20}
+db.replaceOne(taskA);           # <-- Propagate to db
+print([db.findOne(taskA._id).text, taskA.x])
+# Output: ['New AA', {'y': 10, 'z': 20}]
+
+# Increment within document:
+db.incr({"_id": "a"}, ["x", "y"], 1) # Incr x.y by 1
+print(db.findOne("a").x)
+# Output: {'y': 11, 'z': 20}
+
+# Decrement:
+db.decr({"_id": "a"}, ["x", "z"], 1) # Decr x.z by 1
+print(db.findOne("a").x)
+# Output: {'y': 11, 'z': 19}
+```
+
+Deleting Data:
+------------------
+In continuation with the above code snippet ...
+```py
+# Delete by _id:
+db.deleteOne("a");
+print(db.findOne("a"))
+# Output: None
+```
+As of writing, you can only delete one document at a time, by `_id`.
+
+Under The Hood
+---------------------
+Under the hood, PogoDB creates a single table named `pogo_tbl` with a single `JSONB` column named `doc` (for document).
+
+**TODO:** Write documentation regarding lower-level functions such as `db.findSql(.)` and `db.execute(.)`.
+
+Type Identifiers
+-------------------
+
+PogoDB doesn't include buckets, collections or other such concepts for **logically grouping** different types of documents. But you can *(and should)* use a key for differentiating objects of various types.
+
+**Convention:**  
+Keeping things simple, we recommend using the `"type"` key for indicating the type of the object.
+
+**Example:**  
+In a blogging app, you'll have to deal with users, posts, comments and other types of objects. 
+
+```py
+# Insert users:
+db.insertMany([
+    {"_id": "00", "type":"user", "name": "Alice"},
+    {"_id": "01", "type":"user", "name": "Becci"},
+    {"_id": "02", "type":"user", "name": "Cathy"},
+]);
+
+# Insert posts:
+db.insertMany([
+    {"_id": "03", "type":"post", "authorId": "00",
+        "title": "Title X .. ", "body": "Body X .."},
+    {"_id": "04", "type":"post", "authorId": "01",
+        "title": "Title Y .. ", "body": "Body Y .."},
+    {"_id": "05", "type":"post", "authorId": "02",
+        "title": "Title Z .. ", "body": "Body Z .."},
+    {"_id": "06", "type":"post", "authorId": "00",
+        "title": "Title A .. ", "body": "Body A .."},
+]);
+
+# Insert comments:
+db.insertMany([
+    {"_id": "07", "type":"comment", "authorId": "02",
+        "postId": "03", "text": "Comment P .."},
+    {"_id": "08", "type":"comment", "authorId": "01",
+        "postId": "04", "text": "Comment Q .."},
+    {"_id": "09", "type":"comment", "authorId": "00",
+        "postId": "05", "text": "Comment R .."},
+]);
+
+# Get all users:
+db.find({"type": "user"});
+
+# Get all posts:
+db.find({"type": "post"});
+
+# Get posts by a specific author:
+def getPostByAuthor (userId):
+    return db.find({"type":"post", "authorId":userId})
+
+# Get comments on a specific post:
+def getCommentsByPost (postId):
+    return db.find({"type":"comment", "postId":postId})
+
+# Get comments by a specific user:
+def getCommentsByUser (userId):
+    return db.find({"type":"user", "authorId":userId})
+```
+
+As you can see, using the `"type"` key allows us to limit our query to a specific type. In continuation with the above code snippet, consider ...
+```py
+def typed_getPostById (postId):
+    return db.findOne({"_id": postId, "type": "post"});
+
+def untyped_getPostById (postId):
+    return db.findOne({"_id": postId})
+
+print(typed_getPostById("00"))   # Correct result.
+# Output: None
+
+print(untyped_getPostById("00")) # Weird result.
+# Output: {'_id': '00', 'name': 'Alice', 'type': 'user'} 
+```
+
+In the above example, `"00"` corresponds to Alice's `"user"` object. It's not a `"post"`. Yet `untyped_getPostById(.)` (incorrectly) returns it because it is type-blind.
+
+Licensing
+-----------
+Copyright (c) 2020 Polydojo, Inc.
+
+The software is released "AS IS" under the **Apache License 2.0**, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. Kindly see [LICENSE.txt](https://github.com/polydojo/pogodb/blob/master/LICENSE.txt) for more details.
---
+++ pogodb.py
@@ -0,0 +1,214 @@
+"""
+PogoDB: Simple NoSQL wrapper for Postgres' JSONB type.
+
+Copyright (c) 2020 Polydojo, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+""";
+
+import pprint;
+import functools;
+import json;
+import contextlib;
+
+import psycopg2;
+import psycopg2.extras;
+import dotsi;
+
+mapli = lambda seq, fn: dotsi.List(map(fn, seq));
+
+# Good to know:
+# With cursor_factory=psycopg2.extras.RealDictCursor,
+#   cursor.fetchone() returns an instance of RealDictRow.
+#   And cursor.fetchall() returns a list thereof.
+#
+
+
+def bindConCur (con, cur):
+    db = dotsi.fy({"_con": con, "_cur": cur});  # Mainatain ref.
+
+    def execute (stmt, args=None, fetch=None):
+        cur.execute(stmt, args);
+        if fetch in ["one", 1]:
+            # cur.fetchone() -> psycopg2.extras.RealDictRow
+            return dotsi.Dict(cur.fetchone());
+        if fetch == "all":
+            # cur.fetchall -> list of psycopg2.extras.RealDictRow
+            return mapli(cur.fetchall(), dotsi.Dict);
+        return None;
+    db.execute = execute;
+    
+    def ensureTable ():
+        stmtList = [
+            """
+            CREATE TABLE IF NOT EXISTS pogotbl (
+                doc JSONB NOT NULL,
+                CONSTRAINT _id_str_ CHECK (
+                    (doc->'_id') IS NOT NULL
+                        AND
+                    jsonb_typeof(doc->'_id') = 'string'
+                )
+            );
+            """,
+            "CREATE UNIQUE INDEX IF NOT EXISTS _id_unq ON pogotbl ((doc->'_id'));",
+        ];
+        for stmt in stmtList:
+            execute(stmt);
+        return None;
+    db.ensureTable = ensureTable;
+    
+    def showTables ():
+        stmt = "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';";
+        pprint.pprint(execute(stmt, fetch="all"));
+    db.showTables = showTables;
+    
+    def dropTable ():
+        stmt = "DROP TABLE IF EXISTS pogotbl;";
+        execute(stmt);
+    db.dropTable = dropTable;
+    
+    def clearTable ():
+        dropTable();
+        ensureTable();
+    db.clearTable = clearTable;
+        
+    def insertOne (doc):
+        doc = dotsi.fy(doc);
+        stmt = "INSERT INTO pogotbl (doc) VALUES (%s);";
+        execute(stmt, [json.dumps(doc)]);
+    db.insertOne = insertOne;
+    db.insertMany = lambda docList: mapli(docList, insertOne);
+    
+    def replaceOne (doc):
+        doc = dotsi.fy(doc);
+        stmt = "UPDATE pogotbl SET doc = %s WHERE doc->>'_id' = %s";
+        execute(stmt, [json.dumps(doc), doc._id]);
+    db.replaceOne = replaceOne;
+    db.replaceMany = lambda docList: mapli(docList, replaceOne);
+    
+    def deleteOne (docId):
+        stmt = "DELETE FROM pogotbl WHERE doc->>'_id' = %s";
+        execute(stmt, [docId]);
+    db.deleteOne = deleteOne;
+    
+    def findSql (sql, args=None):
+        return mapli(
+            execute(sql, args, fetch="all"),
+            lambda record: record["doc"],
+        );
+    db.findSql = findSql;
+
+    def find (subdoc, whereEtc="", argsEtc=None, limit=None):
+        # Checks:
+        assert isinstance(subdoc, dict) or type(subdoc) is str;
+        assert type(whereEtc) is str;
+        assert argsEtc is None or type(argsEtc) is list;
+        assert limit is None or (type(limit) is int and limit > 0);
+        # Compose:
+        stmt = "\n".join([
+            "SELECT doc FROM pogotbl WHERE doc @> %s",
+            whereEtc,
+            "LIMIT %s" if limit else "",
+            ";",
+        ]);
+        args = (
+            [json.dumps(subdoc)] +
+            (argsEtc or []) +
+            ([limit] if limit else []) #+
+        );
+        # Execute:
+        return findSql(stmt, args);
+    db.find = find;
+            
+    def findById (_id):
+        assert type(_id) is str;
+        docList = findSql("SELECT doc FROM pogotbl WHERE doc->>'_id' = %s", [_id]);
+        assert len(docList) <= 1;
+        return None if not docList else dotsi.fy(docList[0]);
+    db.findById = findById;
+    
+    def findOne (subdoc, whereEtc="", argsEtc=None):
+        if type(subdoc) is str:
+            return findById(subdoc);
+        docList = find(subdoc, whereEtc, argsEtc, limit=1);
+        assert len(docList) <= 1;
+        return None if not docList else docList[0];
+    db.findOne = findOne;
+    
+    def incr (subdoc, keyPath, delta):
+        if type(subdoc) is str: subdoc = {"_id": subdoc};
+        stmt = "UPDATE pogotbl SET doc = jsonb_set(doc, %s, ((doc #> %s)::int + %s)::text::jsonb) WHERE doc @> %s;";
+        args = [keyPath, keyPath,  delta,  json.dumps(subdoc)];
+        execute(stmt, args);
+    db.incr = incr;
+    
+    def decr (subdoc, keyPath, delta):
+        return incr(subdoc, keyPath, -delta);
+    db.decr = decr;
+    
+    def push (subdoc, arrPath, newEl):                      # arrPath: path to array. lastElPath: path to last element _in_ array. 
+        if type(subdoc) is str: subdoc = {"_id": subdoc};
+        lastElPath = arrPath + ["-1"];
+        stmt = "UPDATE pogotbl SET doc = jsonb_insert(doc, %s, %s, true) WHERE doc @> %s;";
+        args = [lastElPath, json.dumps(newEl), json.dumps(subdoc)];
+        execute(stmt, args);
+    db.push = push;
+    
+    # Return built `db`.
+    db.ensureTable();   # TODO: Make this skippable.
+    return db;
+
+@contextlib.contextmanager
+def connect (pgUrl):
+    "Returns a context-managed `db`, bound to `pgUrl`.";
+    con = psycopg2.connect(pgUrl);
+    with con:
+        cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor);
+        with cur:
+            db = bindConCur(con, cur);
+            yield db;
+    #TMP: try: con.close(); except: pass; assert con.closed;
+    con.close();    # Close _OUTSIDE_ the `with con` block;
+    assert con.closed;
+    return None;
+
+def makeConnector (pgUrl):
+    "Returns a `db`-supplying decorator, bound to `pgUrl`.";
+    def dbConnector (fn):
+        @functools.wraps(fn)
+        def wrapper (*args, **kwargs):
+            with connect(pgUrl) as db:
+                return fn(db=db, *args, **kwargs);
+        return wrapper;
+    return dbConnector;
+
+def shellConnect (pgUrl):
+    "Returns context-unmanaged `db`, for use in Python Shell.";
+    db = dotsi.fy({});  # Start empty, maintain reference.
+    def reopen (msg="Connection re-opened. Call `.close()` to close."):
+        con = psycopg2.connect(pgUrl);
+        cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor);
+        db.update(bindConCur(con, cur));
+        if msg: print(msg);
+    db.reopen = reopen;
+    def close (msg="Connection committed & closed. Call `.reopen()` to resume."):
+        db._cur.close();
+        db._con.commit();
+        db._con.close();
+        if msg: print(msg);
+    db.close = close;
+    # Open (1st time) and return:
+    db.reopen("Connection opened. Call `.close()` to close.");
+    return db;
+
+# End ######################################################
